b3d642e6f8a072551650136ea8f1791b6d011c2d
Faraz Hussain <faraz.hussain122@gmail.com>
2025-08-24 19:55:49 +0500

    REFACTOR: code refactor, build issues resolved, consent form integerated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.admobintegerationexample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.admobintegerationexample"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -18,6 +18,15 @@ android {
     }
 
     buildTypes {
+        debug {
+            resValue ("string", "appid", "ca-app-pub-3940256099942544~3347511713")
+            resValue ("string", "banner", "ca-app-pub-3940256099942544/6300978111")
+            resValue ("string", "interads", "ca-app-pub-3940256099942544/1033173712")
+            resValue ("string", "nativeads", "ca-app-pub-3940256099942544/2247696110")
+            resValue ("string", "appopen", "ca-app-pub-3940256099942544/3419835294")
+            resValue ("string", "rewarded_intersiial_ad", "ca-app-pub-3940256099942544/5354046379")
+            resValue ("string", "rewarded_ad", "ca-app-pub-3940256099942544/5224354917")
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
@@ -27,11 +36,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
--- app/src/main/java/com/example/admobintegerationexample/MainActivity.kt
+++ app/src/main/java/com/example/admobintegerationexample/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.admobintegerationexample
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.faraz.admobhelper.ConsentManager
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ConsentManager.ConsentCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,5 +23,14 @@ class MainActivity : AppCompatActivity() {
 
     fun OnShowConsentForm(view: View) {
         val consentManager = ConsentManager()
+        consentManager.setUserConsent(this)
+    }
+
+    override fun onConsentComplete(consentStatus: ConsentManager.ConsentStatus) {
+        if (consentStatus == ConsentManager.ConsentStatus.OBTAINED || consentStatus == ConsentManager.ConsentStatus.NOT_REQUIRED) {
+            Toast.makeText(this, "User can Show Ads", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this, "User can not Show Ads", Toast.LENGTH_SHORT).show()
+        }
     }
 }
